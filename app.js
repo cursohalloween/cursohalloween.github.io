43a7df373ab8a78537e85f3bb898fb03f2ac2a80
Edwin Urquilla <edwin.urquilla@gmail.com>
2015-11-07 11:58:27 -0600

    Clase 2

--- app.js
+++ app.js
@@ -1,34 +1,48 @@
-/*
+var app = angular.module("miApp",[]);
 
-  Ejercicio Numero 1
+app.controller("MiControlador2",function($scope){
+    $scope.var1=0;
+    $scope.var2=0;
+    $scope.resultado = function(){
+        return Number($scope.var1)+Number($scope.var2);
+    }
+});
 
-*/
-
-function censurar(){
-  var comentarioUsuario = document.getElementById("val1")
-  .value
-  var palabras = ["primo","hijo","papa","enojo"]
-
-  //Aca calculamos el reemplazo
-  var resultado = comentarioUsuario;
-  for(var i=0;i<palabras.length;i++){
-    resultado = resultado
-    .replace(new RegExp(palabras[i],"gi"),"****");
-  }
-
-  document.getElementById("resultado").value = resultado;
-}
-
-function alertar(){
-
-  for(var i=0;i<4;i=i+1){
-      alert("Mi alerta");
-  }
-
-
-
-
-
-
-
-}
+app.controller("MiControlador",function($scope,$http){
+    $scope.contactos = []
+    $scope.nombreNuevo = "";
+    $scope.numeroNuevo = "";
+    $scope.editando = -1;
+    
+        
+    $scope.agregar = function(){
+        $scope.contactos.push({
+            nombre:$scope.nombreNuevo,
+            numero:$scope.numeroNuevo
+        });
+        $scope.guardarDatos();
+    }
+    $scope.borrar = function(indice){
+        $scope.contactos.splice(indice,1);
+        $scope.guardarDatos();
+    }
+    $scope.editar = function(indice){
+        $scope.editando = indice;
+    }
+    $scope.guardar = function(){
+        $scope.editando=-1;
+        $scope.guardarDatos();
+    }
+    
+    $scope.recuperarDatos = function(){
+        var contactosSerializados = localStorage.getItem("v1_contactos");
+        if(contactosSerializados != null)
+            $scope.contactos = JSON.parse(contactosSerializados);
+    }
+    
+    $scope.guardarDatos = function(){
+        var contactosSerializados = JSON.stringify($scope.contactos);
+        localStorage.setItem("v1_contactos",contactosSerializados)
+    }
+    $scope.recuperarDatos();
+});
